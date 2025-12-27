07e3efcb1397269639bc3818309e7a02c4e2ef1e
Shaurya Jadaun <shauryajadaun3@gmail.com>
2025-12-27 10:12:40 +0530

    Blueprint register of auth and views

---
+++ website/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    from .auth import auth
+    from .views import views
+
+    app.register_blueprint(auth)
+    app.register_blueprint(views)
+
+    return app
